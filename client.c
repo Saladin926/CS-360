1f9a6578b040eb14f6e7a655459f2530e46e3129
David Ostler <David Ostler>
2016-01-18 23:33:43 -0700

    got it to read everything, but there may be memory leaks all over the place

--- client.c
+++ client.c
@@ -10,6 +10,7 @@
 #define SOCKET_ERROR        -1
 #define BUFFER_SIZE         100
 #define HOST_NAME_SIZE      255
+#define MAXGET 1000
 
 int  main(int argc, char* argv[])
 {
@@ -21,17 +22,18 @@ int  main(int argc, char* argv[])
     unsigned nReadAmount;
     char strHostName[HOST_NAME_SIZE];
     int nHostPort;
+    int totalRead = 0;
 
     if(argc < 6)
-      {
+    {
         printf("\nUsage: myfile host-name host-port path -c or -d\n");
         return 0;
-      }
+    }
     else
-      {
+    {
         strcpy(strHostName,argv[1]);
         nHostPort=atoi(argv[2]);
-      }
+    }
 
     printf("\nMaking a socket\n");
     /* make a socket */
@@ -54,30 +56,55 @@ int  main(int argc, char* argv[])
     Address.sin_family=AF_INET;
 
     /* connect to host */
-    if(connect(hSocket,(struct sockaddr*)&Address,sizeof(Address)) 
-       == SOCKET_ERROR)
+    if(connect(hSocket,(struct sockaddr*)&Address,sizeof(Address))
+    == SOCKET_ERROR)
     {
         printf("\nCould not connect to host\n");
         return 0;
     }
-    
-#define MAXGET 1000
+
+
     //Create HTTP Message
     char* message = (char*)malloc(MAXGET);
     sprintf(message, "GET %s HTTP/1.1\r\nHost:%s:%s\r\n\r\n",argv[3],argv[1],argv[2]);
-    printf("\nRequest: %s\n", message);
+    printf("\nRequest: %s\n~~\n", message);
     //here we are going to allocate space with memory management
     //Send HTTP on the SOcket
     write(hSocket,message,strlen(message));//this is an example of writing a simple line request to the server, we will need to parse
     //Read Response back from Socket
-    nReadAmount=read(hSocket,pBuffer,BUFFER_SIZE);
-    printf("\nResponse: %s\nfrom server. Bytes read: %i\n",pBuffer,nReadAmount);
+    char* wholeMessage = "";
+    do
+    {
+        nReadAmount=read(hSocket,pBuffer,BUFFER_SIZE);
+        if(nReadAmount < BUFFER_SIZE) {
+                pBuffer[nReadAmount] = '\0';
+        }
+        totalRead+=nReadAmount;
+        char* new_str;
+        if((new_str = (char*)malloc(strlen(wholeMessage)+strlen(pBuffer)+1)) != NULL)
+        {
+            new_str[0] = '\0';   // ensures the memory is an empty string
+            strcat(new_str,wholeMessage);
+            strcat(new_str,pBuffer);
+
+            wholeMessage = new_str;
+        }
+        else
+        {
+            printf("\nMalloc for reading in the response absolutely failed!\n");
+            // exit?
+        }
+    }
+    while(nReadAmount >= BUFFER_SIZE);
+
+
+    printf("\nResponse: %s\n~~Bytes read: %i~~\n",wholeMessage,totalRead);
     /* write what we received back to the server */
     //write(hSocket,pBuffer,nReadAmount);
     //printf("\nWriting \"%s\" to server",pBuffer);
 
     printf("\nClosing socket\n");
-    /* close socket */                       
+    /* close socket */
     if(close(hSocket) == SOCKET_ERROR)
     {
         printf("\nCould not close socket\n");
