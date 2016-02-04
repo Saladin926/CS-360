a00a0a52c95efb2f93f99dec4407fa0b7a78da96
David Ostler <David Ostler>
2016-02-03 22:36:00 -0700

    added stuff from help session.  cannot compile yet, but it is a good start

--- webServer/server.c
+++ webServer/server.c
@@ -10,178 +10,208 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
+#include <queue>
 #include <sstream>
+#include <semaphore.h>
 #include "cs360utils.h"
 #define SOCKET_ERROR        -1
 #define BUFFER_SIZE         100
-#define MESSAGE             "This is the message I'm sending back and forth"
-#define QUEUE_SIZE          5
 #define BUFFER_SIZE2         10000
+
 using namespace std;
-void serve(int hSocket,string prepend)
+
+queue<int> work;
+sem_t work_to_do;
+sem_t mutex;
+sem_t space_on_q;
+
+struct thread_params
 {
-    bool servedFile = false;
-    vector<char*> headers;
-        //parse headers
-        string filePath = "";
-        GetHeaderLines(headers, hSocket, false);
-        for(int i = 0; i < headers.size(); i++)
-        { 
-            if(strstr(headers[i], "HTTP_GET") != NULL)
-            {
-                int slashPos = 0;
-                string filePointer = headers[i];
-                slashPos = filePointer.find("/",slashPos);
+    long thread_id;
+    string dir;
+};
 
-                size_t foundSpace = filePointer.find_last_of(" ");
-                slashPos += 1;
-                filePath = filePointer.substr(slashPos,foundSpace - slashPos);
+void* serve(void* arg)
+{
+    long threadID = long(arg);
+    struct thread_params* tp = (struct thread_Params*)arg;
 
-                cout << "This is the file Path: " << filePath << endl;
-            }
-        }
-        filePath = prepend +"/"+ filePath;
-        cout << "File path: " << filePath << endl;
-        //set content type and content length dynamically
-        //parse the get request for file extension
-        //for the content type
-        string fileExt = "";
-        size_t found = filePath.find_last_of(".");
-        fileExt = filePath.substr(found+1);
-
-        string contentType = "";
-
-        if(fileExt == "html")
-        {
-             contentType = "Content-Type: text/html";
-        }
-        else if(fileExt == "txt")
-        {
-            contentType = "Content-Type: text/plain";
-        }
-        else if(fileExt == "jpg")
-        {
-            contentType = "Content-Type: image/jpg";
-        }
-        else if(fileExt == "gif")
-        {
-            contentType = "Content-Type: image/gif";
-        }
-
-        cout << "This should be the content type: " << contentType << endl;
-        stringstream ss;
-        //use stat to determine type of request
+    for(;;)
+    {
+        sem_wait(&work_to_do);
+        sem_wait(&mutex);
         
-        struct stat filestat = {};
-        //check if it's possibly a directory
-        if(filePath == "")
-        {
-            filePath = ".";
-        }
-        const char* charFilePath = filePath.c_str();
-        if(stat(charFilePath, &filestat)) {
-            cout <<"ERROR in stat\n";
-        }
-        if(S_ISREG(filestat.st_mode)) {
-            cout << filePath << " is a regular file \n";
-            cout << "file size = "<<filestat.st_size <<"\n";
-            FILE *fp;
-
-            if(fileExt == "jpg" || fileExt == "gif")
-            {
-                fp = fopen(charFilePath, "rb");
-                
-            }
-            else
-            {
-                fp = fopen(charFilePath, "r");
-
-            }
-            char *buffer = (char*)  malloc(filestat.st_size);
-            fread(buffer, 1, filestat.st_size, fp);
-            //printf("Got\n%s", buffer); //for web server just print to socket instead of the screen
-            ss << "HTTP/1.1 200 OK\r\n" << contentType << "\r\n" << "Content-Length: " << filestat.st_size << "\r\n\r\n";
-            const string headerString = ss.str();
-            cout << headerString << "\n";
-            write(hSocket, headerString.c_str(), headerString.size());
-            write(hSocket, buffer, filestat.st_size);
-            //write(hSocket, buffer, sizeof(buffer));
-            free(buffer);
-            fclose(fp);
-        }
-        if(S_ISDIR(filestat.st_mode)) {
-            cout << filePath << " is a directory \n";
-            DIR* dirp;
-            struct dirent *dp;
-
-            dirp = opendir(charFilePath);
-            char pBuffer[BUFFER_SIZE2];
-            stringstream ssforIndex;
-            bool indexFound = false;
-            while((dp = readdir(dirp)) != NULL)
-            {
-                memset(pBuffer,0,sizeof(pBuffer));
-                printf("<a href = \"%s\">%s</a><br>\n",dp->d_name,dp->d_name); 
-                sprintf(pBuffer,"<a href = \"%s\">%s</a><br>\n",dp->d_name,dp->d_name);
-                ssforIndex << pBuffer;
-                if(strcmp(dp->d_name, "index.html") == 0)
+        int my_conn = work.front();
+        work.pop();
+        
+        cout << "working on " << tp.threadID << " working on "<< my_conn << endl;
+        
+        sem_post(&mutex);
+        sem_post(&space_on_q);
+        //perform normal request serving stuff
+            bool servedFile = false;
+            vector<char*> headers;
+            //parse headers
+            string filePath = "";
+            GetHeaderLines(headers, my_conn, false);
+            for(int i = 0; i < headers.size(); i++)
+            { 
+                if(strstr(headers[i], "HTTP_GET") != NULL)
                 {
-                    indexFound = true;
-                    break;
+                    int slashPos = 0;
+                    string filePointer = headers[i];
+                    slashPos = filePointer.find("/",slashPos);
+
+                    size_t foundSpace = filePointer.find_last_of(" ");
+                    slashPos += 1;
+                    filePath = filePointer.substr(slashPos,foundSpace - slashPos);
+
+                    cout << "This is the file Path: " << filePath << endl;
                 }
             }
-            if(indexFound == true)
+            filePath = tp.dir +"/"+ filePath;
+            cout << "File path: " << filePath << endl;
+            //set content type and content length dynamically
+            //parse the get request for file extension
+            //for the content type
+            string fileExt = "";
+            size_t found = filePath.find_last_of(".");
+            fileExt = filePath.substr(found+1);
+
+            string contentType = "";
+
+            if(fileExt == "html")
             {
-                struct stat newfilestat = {};
-                string newcharFilePath = filePath + "/index.html";
-                const char* nCharFilePath = newcharFilePath.c_str();
-
-                if(stat(nCharFilePath, &newfilestat)) {
-                cout <<"ERROR in stat\n";
-                }
-                if(S_ISREG(newfilestat.st_mode)) {
-                   
-                    FILE *fp = fopen(nCharFilePath, "r");
-
-                    char *buffer = (char*)  malloc(newfilestat.st_size);
-                    fread(buffer, 1, newfilestat.st_size, fp);
-                    //printf("Got\n%s", buffer); //for web server just print to socket instead of the screen
-                    ss << "HTTP/1.1 200 OK\r\n" << "Content-Type: text/html" << "\r\n" << "Content-Length: " << newfilestat.st_size << "\r\n\r\n";
-                    const string headerString = ss.str();
-                    cout << headerString << "\n";
-                    write(hSocket, headerString.c_str(), headerString.size());
-                    write(hSocket, buffer, newfilestat.st_size);
-                    //write(hSocket, buffer, sizeof(buffer));
-                    free(buffer);
-                    fclose(fp);
-                }
-
+                 contentType = "Content-Type: text/html";
             }
-            else
+            else if(fileExt == "txt")
             {
-                stringstream headersForIndex;
-                string findIndex = ssforIndex.str();
-                headersForIndex << "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n" << "Content-Length: " << findIndex.size() << "\r\n\r\n";
-                write(hSocket, headersForIndex.str().c_str(), headersForIndex.str().size());
-                write(hSocket, findIndex.c_str(), findIndex.size());
+                contentType = "Content-Type: text/plain";
             }
+            else if(fileExt == "jpg")
+            {
+                contentType = "Content-Type: image/jpg";
+            }
+            else if(fileExt == "gif")
+            {
+                contentType = "Content-Type: image/gif";
+            }
+
+            cout << "This should be the content type: " << contentType << endl;
+            stringstream ss;
+            //use stat to determine type of request
             
-            servedFile = true;
-            (void)closedir(dirp);
-        } 
-        if(servedFile == false)
-        {
+            struct stat filestat = {};
+            //check if it's possibly a directory
+            if(filePath == "")
+            {
+                filePath = ".";
+            }
+            const char* charFilePath = filePath.c_str();
+            if(stat(charFilePath, &filestat)) {
+                cout <<"ERROR in stat\n";
+            }
+            if(S_ISREG(filestat.st_mode)) {
+                cout << filePath << " is a regular file \n";
+                cout << "file size = "<<filestat.st_size <<"\n";
+                FILE *fp;
 
-            string notFound = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\nContent-Length: 13\r\n\r\n404 Not Found";
-            write(hSocket, notFound.c_str(), notFound.size());
-        }
-        //- folder
-        //- regular file
-        //- invalid (return 404)
-        //if not valid, return canned 404 response
-        //if valid file, use stat to find file size
-        //if folder, return index.html if it exists, if not return a directory listing (html links)
-        
+                if(fileExt == "jpg" || fileExt == "gif")
+                {
+                    fp = fopen(charFilePath, "rb");
+                    
+                }
+                else
+                {
+                    fp = fopen(charFilePath, "r");
+
+                }
+                char *buffer = (char*)  malloc(filestat.st_size);
+                fread(buffer, 1, filestat.st_size, fp);
+                //printf("Got\n%s", buffer); //for web server just print to socket instead of the screen
+                ss << "HTTP/1.1 200 OK\r\n" << contentType << "\r\n" << "Content-Length: " << filestat.st_size << "\r\n\r\n";
+                const string headerString = ss.str();
+                cout << headerString << "\n";
+                write(my_conn, headerString.c_str(), headerString.size());
+                write(my_conn, buffer, filestat.st_size);
+                //write(my_conn, buffer, sizeof(buffer));
+                free(buffer);
+                fclose(fp);
+            }
+            if(S_ISDIR(filestat.st_mode)) {
+                cout << filePath << " is a directory \n";
+                DIR* dirp;
+                struct dirent *dp;
+
+                dirp = opendir(charFilePath);
+                char pBuffer[BUFFER_SIZE2];
+                stringstream ssforIndex;
+                bool indexFound = false;
+                while((dp = readdir(dirp)) != NULL)
+                {
+                    memset(pBuffer,0,sizeof(pBuffer));
+                    printf("<a href = \"%s\">%s</a><br>\n",dp->d_name,dp->d_name); 
+                    sprintf(pBuffer,"<a href = \"%s\">%s</a><br>\n",dp->d_name,dp->d_name);
+                    ssforIndex << pBuffer;
+                    if(strcmp(dp->d_name, "index.html") == 0)
+                    {
+                        indexFound = true;
+                        break;
+                    }
+                }
+                if(indexFound == true)
+                {
+                    struct stat newfilestat = {};
+                    string newcharFilePath = filePath + "/index.html";
+                    const char* nCharFilePath = newcharFilePath.c_str();
+
+                    if(stat(nCharFilePath, &newfilestat)) {
+                    cout <<"ERROR in stat\n";
+                    }
+                    if(S_ISREG(newfilestat.st_mode)) {
+                       
+                        FILE *fp = fopen(nCharFilePath, "r");
+
+                        char *buffer = (char*)  malloc(newfilestat.st_size);
+                        fread(buffer, 1, newfilestat.st_size, fp);
+                        //printf("Got\n%s", buffer); //for web server just print to socket instead of the screen
+                        ss << "HTTP/1.1 200 OK\r\n" << "Content-Type: text/html" << "\r\n" << "Content-Length: " << newfilestat.st_size << "\r\n\r\n";
+                        const string headerString = ss.str();
+                        cout << headerString << "\n";
+                        write(my_conn, headerString.c_str(), headerString.size());
+                        write(my_conn, buffer, newfilestat.st_size);
+                        //write(my_conn, buffer, sizeof(buffer));
+                        free(buffer);
+                        fclose(fp);
+                    }
+
+                }
+                else
+                {
+                    stringstream headersForIndex;
+                    string findIndex = ssforIndex.str();
+                    headersForIndex << "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n" << "Content-Length: " << findIndex.size() << "\r\n\r\n";
+                    write(my_conn, headersForIndex.str().c_str(), headersForIndex.str().size());
+                    write(my_conn, findIndex.c_str(), findIndex.size());
+                }
+                
+                servedFile = true;
+                (void)closedir(dirp);
+            } 
+            if(servedFile == false)
+            {
+
+                string notFound = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\nContent-Length: 13\r\n\r\n404 Not Found";
+                write(my_conn, notFound.c_str(), notFound.size());
+            }
+
+            /* close socket */
+            if(close(my_conn) == SOCKET_ERROR)
+            {
+                 printf("\nCould not close socket\n");
+                 return 0;
+            }
+    }
        
 }
 
@@ -193,23 +223,39 @@ int main(int argc, char* argv[])
     int nAddressSize=sizeof(struct sockaddr_in);
     char pBuffer[BUFFER_SIZE];
     int nHostPort;
+    int threadAmount = argv[2];
+    int queue_size = threadAmount;//queue size is whatever is passed in from the terminal
+    sem_init(&space_on_q,0, queue_size);
+    sem_init(&work_to_do, 0,0);
+    sem_init(&mutex,0, 1);
+    long threadID;
+    pthread_t threads[threadAmount];
+    string dir = argv[3];
 
     if(argc < 2)
-      {
+    {
         printf("\nUsage: server host-port\n");
         return 0;
-      }
+    }
     else
-      {
+    {
         nHostPort=atoi(argv[1]);
-      }
+    }
+
+    for(threadID = 0; threadID < threadAmount; threadID++)
+    {
+        struct thread_params tp;
+        tp.threadID = threadID;
+        tp.dir = dir;
+
+        int ret_val = pthread_create(&threads[threadID],NULL, serve(), (void *)&tp);
+    }
 
     printf("\nStarting server");
 
     printf("\nMaking socket");
     /* make a socket */
     hServerSocket=socket(AF_INET,SOCK_STREAM,0);
-
     if(hServerSocket == SOCKET_ERROR)
     {
         printf("\nCould not make a socket\n");
@@ -231,10 +277,9 @@ int main(int argc, char* argv[])
         printf("\nCould not connect to host\n");
         return 0;
     }
- /*  get port number */
+    /*  get port number */
     getsockname( hServerSocket, (struct sockaddr *) &Address,(socklen_t *)&nAddressSize);
     printf("opened socket as fd (%d) on port (%d) for stream i/o\n",hServerSocket, ntohs(Address.sin_port) );
-
         printf("Server\n\
               sin_family        = %d\n\
               sin_addr.s_addr   = %d\n\
@@ -244,10 +289,9 @@ int main(int argc, char* argv[])
               , ntohs(Address.sin_port)
             );
 
-
-    printf("\nMaking a listen queue of %d elements",QUEUE_SIZE);
+    printf("\nMaking a listen queue of %d elements",queue_size);
     /* establish listen queue */
-    if(listen(hServerSocket,QUEUE_SIZE) == SOCKET_ERROR)
+    if(listen(hServerSocket,queue_size) == SOCKET_ERROR)
     {
         printf("\nCould not listen\n");
         return 0;
@@ -259,21 +303,17 @@ int main(int argc, char* argv[])
         /* get the connected socket */
         hSocket=accept(hServerSocket,(struct sockaddr*)&Address,(socklen_t *)&nAddressSize);
 
+        sem_wait(&space_on_q);
+        sem_wait(&mutex);
+        cout << "pushed " << hSocket << endl;
+        work.push(hSocket);
+        sem_post(&mutex);
+        sem_post(&work_to_do);
+
         printf("\nGot a connection from %X (%d)\n",
         Address.sin_addr.s_addr,
         ntohs(Address.sin_port));
-        memset(pBuffer, 0, sizeof(pBuffer));
-        //read(hSocket,pBuffer,BUFFER_SIZE);
-        printf("got from browser \n %s\n",pBuffer);
-
-        //call server function
-        serve(hSocket,argv[2]);
-       
-        /* close socket */
-        if(close(hSocket) == SOCKET_ERROR)
-        {
-         printf("\nCould not close socket\n");
-         return 0;
-        }
     }
+
+
 }
