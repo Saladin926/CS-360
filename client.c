78b7dca0bee3428f1a22f379028dd48e53c55286
David Ostler <David Ostler>
2016-02-15 22:18:10 -0700

    first test for lab 4

--- client.c
+++ client.c
@@ -1,3 +1,4 @@
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -6,165 +7,131 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <sys/epoll.h>
+#include <sys/time.h>
+#include <iostream>
 
 #define SOCKET_ERROR        -1
-#define BUFFER_SIZE         100
+#define BUFFER_SIZE         10000
 #define HOST_NAME_SIZE      255
-#define MAXGET 1000
+#define NSOCKETS 10
+#define NSTD 3
 #define PAGESIZE 700
+
 int  main(int argc, char* argv[])
 {
-        int hSocket;                 /* handle to socket */
-        struct hostent* pHostInfo;   /* holds info about a machine */
-        struct sockaddr_in Address;  /* Internet socket address stuct */
-        long nHostAddress;
-        char pBuffer[BUFFER_SIZE];
-        unsigned nReadAmount;
-        char strHostName[HOST_NAME_SIZE];
-        int nHostPort;
-        int totalRead = 0;
-        char page[PAGESIZE];
+    int numSockets;
+    struct hostent* pHostInfo;   /* holds info about a machine */
+    struct sockaddr_in Address;  /* Internet socket address stuct */
+    long nHostAddress;
+    char pBuffer[BUFFER_SIZE];
+    unsigned nReadAmount;
+    char strHostName[HOST_NAME_SIZE];
+    int nHostPort;
+    char page[PAGESIZE];
+    bool printAll = false;
 
-        if(argc > 6 || argc < 4)
+    if(argc > 6 || argc < 4)
+    {
+        printf("\nUsage: webtest host port path count [-d]\n");
+        return 0;
+    }
+    else
+    {
+        strcpy(strHostName,argv[1]);
+        nHostPort=atoi(argv[2]);
+        strcpy(page,argv[3]);
+        numSockets = atoi(argv[4]);
+    }
+    int c;
+    int err = 0;
+    while((c = getopt(argc, argv, "d")) !=-1)
+    {
+        switch(c)
         {
-                printf("\nUsage: download [-c count | -d] host-name host-port path \n");
-                return 0;
+            case 'd':
+                printAll = true;
+                break;
+            case '?':
+                err = 1;
+                break;
         }
+    }
+    int hSocket[numSockets];                 /* handle to socket */
+    struct timeval oldtime[numSockets+NSTD];
 
-        extern char* optarg;
-        int c;
-        int times_to_download = 1;
-        int err = 0;
-        bool debug = false;
-        bool cflag = false;//cflag means to display body
+    printf("\nMaking a socket");
+    /* make a socket */
+    for(int i = 0; i < numSockets; i++) {
+        hSocket[i]=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
 
-        while((c = getopt(argc, argv, "c:d")) !=-1)
+        if(hSocket[i] == SOCKET_ERROR)
         {
-                switch(c)
-                {
-                        case 'c':
-                                times_to_download = atoi(optarg);
-                                cflag = true;
-                                break;
-                        case 'd':
-                                debug = true;
-                                break;
-                        case '?':
-                                err = 1;
-                                break;
-                }
+            printf("\nCould not make a socket\n");
+            return 0;
         }
+    }
 
-        for(int i = 0; i < strlen(argv[optind+1]) ; i++)
-        {
-                if(!isdigit(argv[optind+1][i]))
-                {
-                        printf("\nInvalid port\n");
-                        return 0;
-                }
+    /* get IP address from name */
+    pHostInfo=gethostbyname(strHostName);
+    /* copy address into long */
+    memcpy(&nHostAddress,pHostInfo->h_addr,pHostInfo->h_length);
 
-        }
-        nHostPort=atoi(argv[optind+1]);
-        strcpy(page,argv[optind+2]);
-        strcpy(strHostName,argv[optind]);
+    /* fill address struct */
+    Address.sin_addr.s_addr=nHostAddress;
+    Address.sin_port=htons(nHostPort);
+    Address.sin_family=AF_INET;
 
-        //printf("\nMaking a socket\n");
-        /* make a socket */
-        hSocket=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
-
-        if(hSocket == SOCKET_ERROR)
-        {
-                printf("\nCould not make a socket\n");
-                return 0;
-        }
-
-        /* get IP address from name */
-
-        pHostInfo=gethostbyname(strHostName);
-        if(pHostInfo == NULL)
-        {
-                printf("\nInvalid host name\n");
-                return 0;
-        }
-        /* copy address into long */
-        memcpy(&nHostAddress,pHostInfo->h_addr,pHostInfo->h_length);
-
-        /* fill address struct */
-        Address.sin_addr.s_addr=nHostAddress;
-        Address.sin_port=htons(nHostPort);
-        Address.sin_family=AF_INET;
-
+    int epollFD = epoll_create(1);
+    // Send the requests and set up the epoll data
+    for(int i = 0; i < numSockets; i++) {
         /* connect to host */
-        if(connect(hSocket,(struct sockaddr*)&Address,sizeof(Address))
-        == SOCKET_ERROR)
+        if(connect(hSocket[i],(struct sockaddr*)&Address,sizeof(Address)) 
+           == SOCKET_ERROR)
         {
-                printf("\nCould not connect to host\n");
-                return 0;
+            printf("\nCould not connect to host\n");
+            return 0;
         }
-
         //Create HTTP Message
         char* message = (char*)malloc(MAXGET);
         sprintf(message, "GET %s HTTP/1.1\r\nHost:%s:%d\r\n\r\n",page,strHostName,nHostPort);
+        
 
-        //here we are going to allocate space with memory management
-        //Send HTTP on the SOcket
-        for(int i = 0 ; i < times_to_download ; i++)
-        {
-                write(hSocket,message,strlen(message));//this is an example of writing a simple line request to the server, we will need to parse
-        }
+            write(hSocket[i],message,strlen(message));
+        // Keep track of the time when we sent the request
+        gettimeofday(&oldtime[hSocket[i]],NULL);
+        // Tell epoll that we want to know when this socket has data
+        struct epoll_event event;
+        event.data.fd = hSocket[i];
+        event.events = EPOLLIN;
+        int ret = epoll_ctl(epollFD,EPOLL_CTL_ADD,hSocket[i],&event);
+        if(ret)
+            perror ("epoll_ctl");
+    }
+    for(int i = 0; i < numSockets; i++) {
+        struct epoll_event event;
+        int rval = epoll_wait(epollFD,&event,1,-1);
+        if(rval < 0)
+            perror("epoll_wait");
+        rval = read(event.data.fd,pBuffer,BUFFER_SIZE);
+        struct timeval newtime;
+        // Get the current time and subtract the starting time for this request.
+        gettimeofday(&newtime,NULL);
+        double usec = (newtime.tv_sec - oldtime[event.data.fd].tv_sec)*(double)1000000+(newtime.tv_usec-oldtime[event.data.fd].tv_usec);
+        std::cout << "Time "<<usec/1000000<<std::endl;
+        printf("got %d from %d\n",rval,event.data.fd);
+        // Take this one out of epoll
+        epoll_ctl(epollFD,EPOLL_CTL_DEL,event.data.fd,&event);
 
-        //Read Response back from Socket
-        char* wholeMessage = (char*)malloc(1);
-        wholeMessage[0] = '\0';
-        do
-        {
-                nReadAmount=read(hSocket,pBuffer,BUFFER_SIZE);
-                if(nReadAmount < BUFFER_SIZE)
-                {
-                        pBuffer[nReadAmount] = '\0';
-                }
-                totalRead+=nReadAmount;
-                char* new_str;
-                if((new_str = (char*)malloc(strlen(wholeMessage)+strlen(pBuffer)+1)) != NULL)
-                {
-                        new_str[0] = '\0';   // ensures the memory is an empty string
-                        strcat(new_str,wholeMessage);
-                        strcat(new_str,pBuffer);
-
-                        free(wholeMessage);
-                        wholeMessage = (char*)malloc(strlen(new_str) + 1);
-                        strcpy(wholeMessage, new_str);
-                }
-                else
-                {
-                        printf("\nMalloc for reading in the response absolutely failed!\n");
-                        // exit?
-                }
-                free(new_str);
-        }
-        while(nReadAmount >= BUFFER_SIZE);
-
-        const char* delimeter = "\r\n\r\n";
-        char* body = strstr(wholeMessage,delimeter);
-        body[0] = '\0';
-        body += 3;
-        if(debug == true)
-        {
-            printf("Request: %s\n", message);
-            printf("Header: %s\n",wholeMessage);
-        }
-        if(cflag == false)
-        {
-            printf("\n%s\n",body);
-
-
-        }
-
-        /* close socket */
-        if(close(hSocket) == SOCKET_ERROR)
+    }
+    // Now close the sockets
+    for(int i = 0; i < numSockets; i++) {
+        printf("\nClosing socket\n");
+        /* close socket */                       
+        if(close(hSocket[i]) == SOCKET_ERROR)
         {
             printf("\nCould not close socket\n");
             return 0;
         }
+    }
 }
